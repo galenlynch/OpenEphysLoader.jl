b466e19d112ae216d3b894f44bd0f7ba703a0c56
Galen Lynch <glynch@mit.edu>
2016-12-10 09:23:25 -0500

    Remove redundant code in test/util.jl

--- test/util.jl
+++ test/util.jl
@@ -1,26 +1,11 @@
-function filecontext(file_reader::Function, file_writer::Function, args::Any...; kwargs::Any...)
-    path, io = mktemp()
-    try
-        file_writer(io, args...; kwargs...)
-        close(io)
-        io = open(path)
-        file_reader(io)
-    finally
-        close(io)
-        rm(path)
-    end
+function filecontext(file_reader::Function, args::Any...; kwargs::Any...)
+    wrapped_reader(::AbstractString, io::IOStream) = file_reader(io)
+    pathiocontext(wrapped_reader, args...; kwargs...)
 end
 
-function pathcontext(file_reader::Function, file_writer::Function, args::Any...; kwargs::Any...)
-    path, io = mktemp()
-    try
-        file_writer(io, args...; kwargs...)
-        close(io)
-        file_reader(path)
-    finally
-        close(io)
-        rm(path)
-    end
+function pathcontext(file_reader::Function, args::Any...; kwargs::Any...)
+    wrapped_reader(path::AbstractString, ::IOStream) = file_reader(path)
+    pathiocontext(wrapped_reader, args...; kwargs...)
 end
 
 function pathiocontext(file_reader::Function, file_writer::Function, args::Any...; kwargs::Any...)
