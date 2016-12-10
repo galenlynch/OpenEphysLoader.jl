fc3eb971241943e19247cc58f2486f1259f06fb9
Galen Lynch <glynch@mit.edu>
2016-12-10 10:25:27 -0500

    Change argument types of test/util.jl functions

--- test/util.jl
+++ test/util.jl
@@ -1,14 +1,14 @@
-function filecontext(file_reader::Function, args::Any...; kwargs::Any...)
+function filecontext(file_reader::Function, args...; kwargs...)
     wrapped_reader(::AbstractString, io::IOStream) = file_reader(io)
     pathiocontext(wrapped_reader, args...; kwargs...)
 end
 
-function pathcontext(file_reader::Function, args::Any...; kwargs::Any...)
+function pathcontext(file_reader::Function, args...; kwargs...)
     wrapped_reader(path::AbstractString, ::IOStream) = file_reader(path)
     pathiocontext(wrapped_reader, args...; kwargs...)
 end
 
-function pathiocontext(file_reader::Function, file_writer::Function, args::Any...; kwargs::Any...)
+function pathiocontext(file_reader::Function, file_writer::Function, args...; kwargs...)
     path, io = mktemp()
     try
         file_writer(io, args...; kwargs...)
